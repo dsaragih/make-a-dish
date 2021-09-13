4c7b2916016444e6f3c68a521d77e5e8ff0820cd
Daniel Gabriel <HARRY2@MacBook-Air.local>
2021-09-13 18:27:11 -0400

    fixed url links

--- frontend/src/http-db.js
+++ frontend/src/http-db.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://https://make-a-dish.herokuapp.com/",
+    baseURL: "https://make-a-dish.herokuapp.com/",
     headers: {
         "Content-Type": "application/json"
     }
